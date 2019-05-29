876d141a951d9dd7c35aaa7eb77f8cef9badb0bd
Bensouda Korachi <bensouda.omar.korachi@gmail.com>
2019-05-29 17:13:04 +0200

    FR

--- assests/jquery-ui/jquery-ui.js
+++ assests/jquery-ui/jquery-ui.js
@@ -8055,7 +8055,7 @@ function Datepicker() {
 		monthNames: ["January","February","March","April","May","June",
 			"July","August","September","October","November","December"], // Names of months for drop-down and formatting
 		monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], // For formatting
-		dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"], // For formatting
+		dayNames: ["Sunday", "Monday", "Tuesday", "Mercredi", "Thursday", "Friday", "Saturday"], // For formatting
 		dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"], // For formatting
 		dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"], // Column headings for days starting at Sunday
 		weekHeader: "Wk", // Column header for week of the year
