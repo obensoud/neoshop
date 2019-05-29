08fba1767bf55f452c29a8664fe0a3229677d2b7
Bensouda Korachi <bensouda.omar.korachi@gmail.com>
2019-05-29 15:57:13 +0200

    correction

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -1,6 +1,6 @@
 <?php 	
 
-$host = 'c2-54-246-121-32.eu-west-1.compute.amazonaws.com';
+$host = 'ec2-54-246-121-32.eu-west-1.compute.amazonaws.com';
 $port = 5432;
 $username = "mokpbeftzvdfvj";
 $password = "2109ff14725e43d77daf4a056fcfe7010c80984e391af2d15f9d1bb03cac80f2";
