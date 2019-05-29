bf5908c078ca8fd7a3ef1770251f380741918631
Bensouda Korachi <bensouda.omar.korachi@gmail.com>
2019-05-29 14:18:28 +0200

    Prod

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -1,16 +1,10 @@
 <?php 	
 
-$host = 'localhost';
-$port = 3306;
-$username = "root";
-$password = "root";
-$dbname = "stock";
-// $host = 'mysql.hostinger.fr';
-// $port = 3306;
-// $username = "u293021064_root";
-// $password = "uU0D7vpgbJoj";
-// $dbname = "u293021064_stock";
-// db connection
+$host = 'ec2-176-34-184-174.eu-west-1.compute.amazonaws.com';
+$port = 5432;
+$username = "kvdnxvnmkwgtmh";
+$password = "37cd4d03bd4bb71a90b1d4df7f7fe6a175923c4df89baa03b0078152f6fd9c15";
+$dbname = "d4bsd5rvsqtl7n";
   
   $connect = new mysqli($host, $username, $password, $dbname, $port);
 //$connect = new mysqli($host, $username, $password, $dbname);
